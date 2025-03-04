eeaef25a5d87f3e79e8566477e597a06eb922239
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-04 16:09:01 +0530

    Time: 32 ms (64.74%) | Memory: 56.6 MB (40.95%) - LeetSync

---
+++ 931-maximum-frequency-stack/maximum-frequency-stack.java
@@ -0,0 +1,25 @@
+class FreqStack {
+    Map<Integer, Integer> frq = new HashMap<>();
+    Map<Integer, Stack<Integer>> st_each_freq = new HashMap<>();
+    int maxfr = 0;
+
+    public FreqStack() {
+    }
+
+    public void push(int val) {
+        int f = frq.getOrDefault(val, 0) + 1;
+        frq.put(val, f);
+
+        maxfr = Math.max(f, maxfr);
+        if (!st_each_freq.containsKey(f))
+            st_each_freq.put(f, new Stack<>());
+        st_each_freq.get(f).add(val);
+    }
+
+    public int pop() {
+        int max=st_each_freq.get(maxfr).pop();
+        frq.put(max,maxfr-1);
+        if(st_each_freq.get(maxfr).size()==0)maxfr--;
+        return max;
+    }
+}
