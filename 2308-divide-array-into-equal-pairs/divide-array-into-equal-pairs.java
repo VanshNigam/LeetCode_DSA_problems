408ac16dfcfa2b620c4125db266f7be5decb6d50
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-17 09:56:56 +0530

    Time: 6 ms (45.22%) | Memory: 44.9 MB (13.78%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,13 +1,33 @@
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         Map<Integer, Integer> mp = new HashMap<>();
+
+//         for (int i : nums)
+//             mp.put(i, mp.getOrDefault(i, 0) + 1);
+
+//         for (int i : mp.keySet())
+//             if (mp.get(i) % 2 != 0)
+//                 return false;
+//         return true;
+//     }
+// }
+
 class Solution {
+
     public boolean divideArray(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
+        // Track unpaired numbers
+        Set<Integer> unpaired = new HashSet<>();
 
-        for (int i : nums)
-            mp.put(i, mp.getOrDefault(i, 0) + 1);
+        // Add numbers to set if unseen, remove if seen
+        for (int num : nums) {
+            if (unpaired.contains(num)) {
+                unpaired.remove(num);
+            } else {
+                unpaired.add(num);
+            }
+        }
 
-        for (int i : mp.keySet())
-            if (mp.get(i) % 2 != 0)
-                return false;
-        return true;
+        // Return true if all numbers were paired
+        return unpaired.isEmpty();
     }
 }
