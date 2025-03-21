f45a1117182216b5c18dedd22d7a81df95638f01
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-21 15:39:00 +0530

    Time: 7 ms (38.83%) | Memory: 41.9 MB (91.49%) - LeetSync

---
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int gcd(int x, int y) {
+        int x1 = Math.max(x, y);
+        int y1 = Math.min(x, y);
+        while (y1 != 0) {
+            int t = x1 % y1;
+            x1 = y1;
+            y1 = t;
+        }
+        return x1;
+    }
+
+    public int subarrayGCD(int[] nums, int k) {
+        int c = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int g = nums[i];
+            if (g == k)
+                c++;
+            for (int j = i + 1; j < nums.length; j++) {
+                g = gcd(g, nums[j]);
+
+                if (g == k)
+                    c++;
+                if (g < k)
+                    break;
+            }
+        }
+        return c;
+    }
+}
