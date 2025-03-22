ede6e1ce85b46bac0fe731ff7db1f09c41eced61
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-03-23 02:37:40 +0530

    Time: 15 ms (11.06%) | Memory: 48.3 MB (12.72%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,35 @@
+
+class Solution {
+    // int
+    public int help(int[] p, int i, int prevInd, int k, int flag, int dp[][][]) {
+        if (i == p.length || k < 1)
+            return 0;
+        int buy = 0;
+        int notbuy = 0;
+        int sell = 0;
+        int notSell = 0;
+        if (dp[i][k][flag] != -1)
+            return dp[i][k][flag];
+        if (flag == 1) {
+            buy = -p[i] + help(p, i + 1, i, k, 0, dp);
+            notbuy = help(p, i + 1, 0, k, 1, dp);
+        } else {
+            sell = p[i] + help(p, i + 1, 0, k - 1, 1, dp);
+            notSell = help(p, i + 1, prevInd, k, 0, dp);
+        }
+        return dp[i][k][flag] = Math.max(buy, Math.max(sell, Math.max(notbuy, notSell)));
+    }
+
+    public int maxProfit(int k, int[] p) {
+
+        int dp[][][] = new int[p.length][k + 1][2];
+
+        for(int i[][]:dp){
+                for(int x[]:i){
+                    x[0]=-1;
+                    x[1]=-1;
+                }
+        }
+        return help(p, 0, 0, k, 1, dp);
+    }
+}
