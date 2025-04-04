6a87581635eeae4731fa7e5fb45f6697901c06bd
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-04 13:09:13 +0530

    Time: 8 ms (86.82%) | Memory: 46.4 MB (91.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,20 +2,24 @@ class Solution {
     public int[][] merge(int[][] arr) {
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
 
+        ArrayList<int[]> l = new ArrayList<>();
         int prev[] = arr[0];
-        ArrayList<int[]> a = new ArrayList<>();
-
-        for (int i = 1; i < arr.length; i++) {
-            int curr[] = arr[i];
-
-            if (curr[0] <= prev[1]) {
-                prev[1] = Math.max(curr[1], prev[1]);
+        for (int i[] : arr) {
+            if (prev[1] >= i[0]) {
+                prev[1] = Math.max(i[1], prev[1]);
             } else {
-                a.add(prev);
-                prev = curr;
+                l.add(prev);
+                prev = i;
             }
         }
-        a.add(prev);
-        return a.toArray(new int[a.size()][]);
+        l.add(prev);
+
+        int ans[][] = new int[l.size()][2];
+        int k = 0;
+        for (int i[] : l) {
+            ans[k] = i;
+            k++;
+        }
+        return ans;
     }
 }
