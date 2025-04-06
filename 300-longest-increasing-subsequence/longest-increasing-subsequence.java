32d94d64b2f45d4a378ff69d3ea83dd3542b06a7
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-06 21:28:58 +0530

    Time: 61 ms (25.98%) | Memory: 44.3 MB (64.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -25,22 +25,44 @@
 
 //2nd solution
 
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int dp[][] = new int[nums.length+1][nums.length + 1];
+
+//         int n = nums.length;
+//         for (int i = n - 1; i >= 0; i--) {
+//             for (int prev = i - 1; prev >= -1; prev--) {
+//                 int len = 0 + dp[i + 1][prev + 1];
+
+//                 if (prev == -1 || nums[i] > nums[prev])
+//                     len = Math.max(len, 1 + dp[i + 1][i + 1]);
+
+//                 dp[i][prev + 1] = len;
+
+//             }
+//         }
+//         return dp[0][-1 + 1];
+//     }
+// }
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[][] = new int[nums.length+1][nums.length + 1];
+        int next[]= new int[nums.length+1];
+        int curr[]= new int[nums.length+1];
 
         int n = nums.length;
         for (int i = n - 1; i >= 0; i--) {
             for (int prev = i - 1; prev >= -1; prev--) {
-                int len = 0 + dp[i + 1][prev + 1];
+                int len = 0 + next[prev + 1];
 
                 if (prev == -1 || nums[i] > nums[prev])
-                    len = Math.max(len, 1 + dp[i + 1][i + 1]);
+                    len = Math.max(len, 1 + next[i + 1]);
 
-                dp[i][prev + 1] = len;
+                curr[prev + 1] = len;
 
             }
+            next=curr;
         }
-        return dp[0][-1 + 1];
+        return next[-1 + 1];
     }
 }
