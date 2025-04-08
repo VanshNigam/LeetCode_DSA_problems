d75ce0d4396ad34b1793eff57a2d9b97d9879f6e
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-08 09:58:58 +0530

    Time: 1 ms (99.90%) | Memory: 43.3 MB (95.90%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,28 +1,24 @@
 class Solution {
-    Set<List<Integer>> st = new HashSet<>();
+    public void help(int[] nums, int ind, List<Integer> curr, List<List<Integer>> lst) {
+        lst.add(new ArrayList<>(curr));
 
-    public void help(int nums[], int i, List<Integer> lst) {
-        if (i == nums.length) {
-            // Collections.sort(lst);
-
-            st.add(new ArrayList<>(lst));
-            return;
+        for (int i = ind; i < nums.length; i++) {
+            if (i > ind && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            curr.add(nums[i]);
+            
+            help(nums, i + 1, curr, lst);
+            curr.remove(curr.size() - 1);
         }
-        lst.add(nums[i]);
-        help(nums, i + 1, lst);
-
-        lst.remove(lst.size() - 1);
-        help(nums, i + 1, lst);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
+        List<List<Integer>> lst = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
         Arrays.sort(nums);
-        help(nums, 0, new ArrayList<>());
-        for (List<Integer> l : st) {
-            ans.add(l);
-        }
-        return ans;
+
+        help(nums, 0, curr, lst);
+        return lst;
     }
 }
