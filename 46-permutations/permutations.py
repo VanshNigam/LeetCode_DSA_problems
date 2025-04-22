c39408851bf0164693eca5d0593658644774348d
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-22 09:57:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (57.00%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,15 +1,15 @@
-class Solution(object):
-    def permute(self, nums):
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
         def helper(idx):
-            if(idx==len(nums)):
+            if idx == len(nums):
                 res.append(nums[:])
-                return 
+                return
 
-            for i in range(idx,len(nums)):
-                nums[i],nums[idx]=nums[idx],nums[i]
-                helper(idx+1)
-                nums[i],nums[idx]=nums[idx],nums[i]
+            for i in range(idx, len(nums)):
+                nums[i], nums[idx] = nums[idx], nums[i]
+                helper(idx + 1)
+                nums[i], nums[idx] = nums[idx], nums[i]
 
-        res=[]
+        res = []
         helper(0)
         return res
