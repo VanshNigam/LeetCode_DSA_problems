15dcd6ab9fde6de79d6a40e9d3d0974cf5b61a50
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-29 10:14:28 +0530

    Time: 6 ms (37.74%) | Memory: 58.1 MB (95.85%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,21 +1,29 @@
 class Solution {
 
     public long countSubarrays(int[] nums, int k) {
-        int maxElement = Arrays.stream(nums).max().getAsInt();
-        long ans = 0, start = 0;
-        int maxElementsInWindow = 0;
+        // Finding the maximum element in the array
 
-        for (int end = 0; end < nums.length; end++) {
-            if (nums[end] == maxElement) {
-                maxElementsInWindow++;
+        int maxElement = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxElement) {
+                maxElement = nums[i];
             }
-            while (maxElementsInWindow == k) {
-                if (nums[(int) start] == maxElement) {
-                    maxElementsInWindow--;
-                }
-                start++;
+        }
+
+        ArrayList<Integer> indexesOfMaxElements = new ArrayList<>();
+        long ans = 0;
+
+        // Iterating through the array
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == maxElement) {
+                indexesOfMaxElements.add(index);
             }
-            ans += start;
+
+            int freq = indexesOfMaxElements.size();
+            if (freq >= k) {
+                ans += indexesOfMaxElements.get(freq - k) + 1;
+            }
         }
         return ans;
     }
