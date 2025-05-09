58a6812e67c3370fe2e4ac4488f543a262f527f7
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-05-09 15:20:57 +0530

    Time: 99 ms (61.16%) | Memory: 49 MB (83.35%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -4,7 +4,7 @@ class Solution {
         boolean prime[] = new boolean[n + 2];
         prime[0] = prime[1] = true;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i = i + 1) {
             if (!prime[i]) {
                 c++;
                 for (int j = i * 2; j < n; j = j + i) {
