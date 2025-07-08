0f5fc0f8e6a78e968709ff0e749b976da7ea8a34
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-08 23:00:47 +0530

    Time: 116 ms (36.06%) | Memory: 135.2 MB (43.75%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,41 +1,37 @@
-//not by me
-
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
-        
-        dp = new int[k + 1][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        
-        return dfs(0, k, events);
-    }    
-    
-    private int[][] dp;
-    private int dfs(int curIndex, int count, int[][] events) {
-        if (count == 0 || curIndex == events.length) {
+    int[][] dp;
+
+    public int help(int[][] e, int i, int k) {
+        if (i >= e.length || k == 0)
             return 0;
-        }
-        if (dp[count][curIndex] != -1) {
-            return dp[count][curIndex];
-        }
-        int nextIndex = bisectRight(events, events[curIndex][1]);
-        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
-        return dp[count][curIndex];
-    }
-    
-    public static int bisectRight(int[][] events, int target) {
-        int left = 0, right = events.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (events[mid][0] <= target) {
-                left = mid + 1;
+        if (dp[i][k] != -1)
+            return dp[i][k];
+
+        // Binary search to find the next event starting after e[i][1]
+        int low = i + 1, high = e.length, next = e.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (e[mid][0] > e[i][1]) {
+                next = mid;
+                high = mid;
             } else {
-                right = mid;
+                low = mid + 1;
             }
         }
-        return left;
-    }   
+
+        int take = e[i][2] + help(e, next, k - 1);
+        int skip = help(e, i + 1, k);
+
+        return dp[i][k] = Math.max(take, skip);
+    }
+
+    public int maxValue(int[][] events, int k) {
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+
+        dp = new int[events.length][k + 1];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return help(events, 0, k);
+    }
 }
