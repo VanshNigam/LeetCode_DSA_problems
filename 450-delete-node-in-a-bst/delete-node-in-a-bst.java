9ab2d4e2cd2d00253eb495af3ba38664812a71e4
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-17 14:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (76.81%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,40 +1,16 @@
 class Solution {
-    public void del(TreeNode r, int k, TreeNode prev) {
-        if (r == null) return;
-
-        if (r.val == k) {
-            if (r.left == null && r.right == null) { // No child
-                if (prev != null) {
-                    if (prev.left == r) prev.left = null;
-                    else prev.right = null;
-                }
-            } else if (r.left != null && r.right == null) { // Only left child
-                if (prev != null) {
-                    if (prev.left == r) prev.left = r.left;
-                    else prev.right = r.left;
-                }
-            } else if (r.right != null && r.left == null) { // Only right child
-                if (prev != null) {
-                    if (prev.left == r) prev.left = r.right;
-                    else prev.right = r.right;
-                }
-            } else { // Two children
-                TreeNode temp = r.right;
-                while (temp.left != null) temp = temp.left;
-                r.val = temp.val;
-                del(r.right, temp.val, r);
-            }
-            return;
-        }
-
-        del(r.left, k, r);
-        del(r.right, k, r);
-    }
-
     public TreeNode deleteNode(TreeNode root, int key) {
-        TreeNode dummy = new TreeNode(0);
-        dummy.left = root;
-        del(root, key, dummy);
-        return dummy.left;
+        if (root == null) return null;
+        if (key < root.val) root.left = deleteNode(root.left, key);
+        else if (key > root.val) root.right = deleteNode(root.right, key);
+        else {
+            if (root.right == null) return root.left;
+            if (root.left == null) return root.right;
+            TreeNode temp = root.right;
+            while (temp.left != null) temp = temp.left;
+            root.val = temp.val;
+            root.right = deleteNode(root.right, temp.val);
+        }
+        return root;
     }
 }
