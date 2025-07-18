025938899d76206d89c992dd92f25f15adfbaa92
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-18 16:09:22 +0530

    Time: 9 ms (69.60%) | Memory: 46.8 MB (43.74%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -1,21 +1,19 @@
 class Solution {
-    public boolean help(String a, String b) {
-        boolean[] freq = new boolean[26];
-
-        for (char c : a.toCharArray())  freq[c - 'a'] = true;
-
-        for (char c : b.toCharArray()) 
-            if (freq[c - 'a'])
-                return true;
-
-        return false;
-    }
-
     public int maxProduct(String[] words) {
+        int arr[] = new int[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            int mask = 0;
+            for (char c : words[i].toCharArray()) {
+                int bit = c - 'a';
+                mask |= (1 << (c - 'a'));
+            }
+            arr[i] = mask;
+        }
         int max = 0;
         for (int i = 0; i < words.length; i++) {
             for (int j = i + 1; j < words.length; j++) {
-                if (!help(words[i], words[j]))
+                if ((arr[i] & arr[j]) == 0)
                     max = Math.max(max, words[i].length() * words[j].length());
             }
         }
