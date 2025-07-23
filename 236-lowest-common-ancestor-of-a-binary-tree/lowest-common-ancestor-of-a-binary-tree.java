eb8d29ae6378566a885bb004518cddcd7b09579d
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-23 14:26:06 +0530

    Time: 16 ms (5.95%) | Memory: 45.5 MB (8.46%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,21 +1,40 @@
 class Solution {
-    public TreeNode help(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null)
-            return null;
-        if (root == p || root == q)
-            return root;
-
-        TreeNode l = help(root.left, p, q);
-        TreeNode r = help(root.right, p, q);
-
-        if (l != null && r != null)
-            return root;
-        else if (l != null)
-            return l;
-        return r;
-    }
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return help(root, p, q);
+        Queue<TreeNode> que = new LinkedList<>();
+        List<TreeNode> l1 = new ArrayList<>();
+        List<TreeNode> l2 = new ArrayList<>();
+        Map<TreeNode, TreeNode> mp = new HashMap<>();
+        que.add(root);
+        mp.put(root, null);//parent save 
+
+        while (!que.isEmpty()) {
+            TreeNode x = que.poll();
+            if (x.left != null) {
+                que.add(x.left);
+                mp.put(x.left, x);
+            }
+            if (x.right != null) {
+                que.add(x.right);
+                mp.put(x.right, x);
+            }
+        }
+        while (p != null) {
+            l1.add(p);
+            p = mp.get(p);
+        }
+        while (q != null) {
+            l2.add(q);
+            q = mp.get(q);
+        }
+        Collections.reverse(l1);
+        Collections.reverse(l2);
+
+        int i = 0;
+        int j = 0;
+        while (i < l1.size() && j < l2.size() && l1.get(i) == l2.get(j)) {
+            i++;
+            j++;
+        }
+        return l1.get(i - 1);
     }
 }
