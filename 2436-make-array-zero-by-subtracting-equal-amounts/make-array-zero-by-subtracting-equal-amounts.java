c23bd91a9849cb31ad0bb214e37146bd72f3b99b
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-30 13:46:11 +0530

    Time: 1 ms (94.61%) | Memory: 41.5 MB (30.20%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for (int i : nums)
+            pq.add(i);
+
+        int sub = 0;
+        int c = 0;
+
+        while (!pq.isEmpty()) {
+            int pop = pq.poll();
+            if (pop - sub == 0)
+                continue;
+            else {
+                sub += (pop - sub);
+                c++;
+            }
+        }
+        return c;
+    }
+}
