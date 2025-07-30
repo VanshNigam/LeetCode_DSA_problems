6fc0453ca67aa9c79450874c26c09348b90fd8f3
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-30 13:58:03 +0530

    Time: 2 ms (26.57%) | Memory: 41 MB (97.17%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,17 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         PriorityQueue<Integer> pq = new PriorityQueue();
-        for (int i : nums)
-            pq.add(i);
-
-        int sub = 0;
-        int c = 0;
-
+        for (int i : nums) pq.add(i);
+        int sub = 0,c = 0;
         while (!pq.isEmpty()) {
             int pop = pq.poll();
-            if (pop - sub == 0)
-                continue;
-            else {
+            if (pop - sub != 0){
                 sub += (pop - sub);
                 c++;
             }
