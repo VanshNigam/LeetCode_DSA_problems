8365179bb3649378f6a92af95e288b7b45839194
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-08-03 21:46:37 +0530

    Time: 3 ms (100.00%) | Memory: 62.4 MB (100.00%) - LeetSync

---
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxBalancedShipments(int[] nums) {
+        int count = 0;
+        int maxSoFar = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        for (int i = 0, start = 0; i < n; i++) {
+            maxSoFar = Math.max(maxSoFar, nums[i]);
+
+            // A valid balanced shipment must have at least 2 elements
+            if (i > start && nums[i] < maxSoFar) {
+                count++;
+                start = i + 1;            // Start new shipment after this
+                maxSoFar = Integer.MIN_VALUE;  // Reset max for new shipment
+            }
+        }
+
+        return count;
+    }
+}
