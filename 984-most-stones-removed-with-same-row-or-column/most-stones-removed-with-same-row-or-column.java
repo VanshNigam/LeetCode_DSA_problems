c03072e3a71d23abbea4c971c910cb66433573cf
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-08 15:02:13 +0530

    Time: 18 ms (63.55%) | Memory: 45.2 MB (46.91%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,6 +1,7 @@
 class DisjointSet {
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+
     public DisjointSet(int n) {
         for (int i = 0; i <= n; i++) {
             rank.add(0);
@@ -20,7 +21,8 @@ class DisjointSet {
     public void unionByRank(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
+        if (ulp_u == ulp_v)
+            return;
         if (rank.get(ulp_u) < rank.get(ulp_v)) {
             parent.set(ulp_u, ulp_v);
         } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
@@ -32,30 +34,32 @@ class DisjointSet {
         }
     }
 }
+
 class Solution {
     public int removeStones(int[][] stones) {
-        int maxRow=0,maxCol=0;
+        int maxRow = 0, maxCol = 0;
 
-        for(int i[]:stones){
-            maxCol=Math.max(maxCol,i[1]);
-            maxRow=Math.max(maxRow,i[0]);
+        for (int i[] : stones) {
+            maxCol = Math.max(maxCol, i[1]);
+            maxRow = Math.max(maxRow, i[0]);
         }
-        DisjointSet ds=new DisjointSet(maxRow+maxCol+1);
-        Map<Integer,Integer>mp=new HashMap<>();
+        DisjointSet ds = new DisjointSet(maxRow + maxCol + 1);
+        Map<Integer, Integer> mp = new HashMap<>();
 
-        for(int i[]:stones){
-            int x=i[0];
-            int y=i[1]+maxRow+1;
+        for (int i[] : stones) {
+            int x = i[0];
+            int y = i[1] + maxRow + 1;
 
-            ds.unionByRank(x,y);
-            mp.put(x,1);
-            mp.put(y,1);
+            ds.unionByRank(x, y);
+            mp.put(x, 1);
+            mp.put(y, 1);
         }
-        int c=0;
-        for(int i:mp.keySet()){
-            if(ds.findUPar(i)==i)c++;
+        int c = 0;
+        for (int i : mp.keySet()) {
+            if (ds.findUPar(i) == i)
+                c++;
         }
 
-        return stones.length-c;
+        return stones.length - c;
     }
 }
