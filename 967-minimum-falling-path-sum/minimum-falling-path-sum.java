918dec40cfada68916e08cfc2b2a2c281048a3de
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-14 13:41:33 +0530

    Time: 1 ms (99.89%) | Memory: 45.5 MB (41.12%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int ans = 1000001;
-        int dp[][] = new int[n][n];
-        for(int []d:dp){
-            Arrays.fill(d,1000001);
-        }
-        for(int j=0;j<n;j++){
-            ans = Math.min(ans,recur(0,j,n,dp,matrix));
-        }
-        return ans;
-    }
-    private int recur(int r,int c,int n,int dp[][],int matrix[][]){
-        if(c==n || c==-1) return 1000001;
-        if(r== n-1){
-            return matrix[r][c];
-        }
-        if(dp[r][c]!=1000001) return dp[r][c];
-        int ans = 1000001;
-        for(int i=-1;i<=1;i++){
-           ans = Math.min(ans,recur(r+1,c+i,n,dp,matrix));
-        }
-        return dp[r][c] = matrix[r][c] + ans;
+    public int helper(int[][] grid, int x, int y, int dp[][]) {
+        if (grid[0].length <= y || y < 0)
+            return Integer.MAX_VALUE;
 
+        if (x == grid.length - 1)
+            return grid[x][y];
+
+        if (dp[x][y] != Integer.MAX_VALUE)
+            return dp[x][y];
+        int back = helper(grid, x + 1, y - 1, dp); // diagonal left-down
+        int down = helper(grid, x + 1, y, dp); // straight down
+        int forw = helper(grid, x + 1, y + 1, dp); // diagonal right-down
+
+        int temp = Math.min(back, Math.min(down, forw)); // take minimum among 3 options
+
+        dp[x][y] = grid[x][y] + temp;
+        return dp[x][y];
+    }
+
+    public int minFallingPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, Integer.MAX_VALUE);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < grid[0].length; i++) // iterate columns, not rows
+            min = Math.min(helper(grid, 0, i, dp), min);
+        return min;
     }
 }
