12f57f79c88103d1195312df5b0a594e6ac9da95
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-14 14:17:04 +0530

    Time: 5 ms (71.39%) | Memory: 45.2 MB (80.05%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -19,29 +19,28 @@ class Solution {
     // }
 
     public int minFallingPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, Integer.MAX_VALUE);
-        }
-        int n=grid.length;
+        int[] dp = new int[grid[0].length];
+        int[] curr = new int[grid[0].length];
+        int n = grid.length;
 
-        for(int i=0;i<n;i++){
-            dp[n-1][i]=grid[n-1][i];
+        for (int i = 0; i < n; i++) {
+            dp[i] = grid[n - 1][i];
         }
 
-        for(int i=n-2;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int back = (j>0 ) ? dp[i+1][j-1] : Integer.MAX_VALUE; 
-                int down = dp[i+1][j] ;
-                int forw = (j<n-1) ? dp[i+1][j+1] : Integer.MAX_VALUE ;
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int back = (j > 0) ? dp[j - 1] : Integer.MAX_VALUE;
+                int down = dp[j];
+                int forw = (j < n - 1) ? dp[j + 1] : Integer.MAX_VALUE;
 
                 int temp = Math.min(back, Math.min(down, forw));
-                dp[i][j] = grid[i][j] + temp; 
+                curr[j] = grid[i][j] + temp;
             }
+            dp = curr.clone();
         }
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            min=Math.min(min,dp[0][i]);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, dp[i]);
         }
         return min;
     }
