070f6501091a946722c76ed06076bef1cac9e776
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-15 13:20:33 +0530

    Time: 34 ms (15.79%) | Memory: 45.6 MB (13.48%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,36 @@
+class Solution {
+    // int min = Integer.MAX_VALUE;
+
+    public int help(int[] coins, int amount, int curr, int i, int dp[][]) {
+        if (i >= coins.length) {
+            if (curr == amount)
+                return 0;
+            return Integer.MAX_VALUE - 1;
+        }
+        if (curr > amount)
+            return Integer.MAX_VALUE - 1;
+
+        if (dp[i][curr] != -1)
+            return dp[i][curr];
+
+        int take = Integer.MAX_VALUE - 1;
+        int not_take = Integer.MAX_VALUE;
+        if (amount - curr >= coins[i])
+            take = 1 + help(coins, amount, curr + coins[i], i, dp);
+        not_take = help(coins, amount, curr, i + 1, dp);
+
+        return dp[i][curr] = Math.min(take, not_take);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        int dp[][] = new int[n][amount + 1];
+
+        for (int i[] : dp)
+            Arrays.fill(i, -1);
+
+        int min = help(coins, amount, 0, 0, dp);
+        return (min >= Integer.MAX_VALUE - 1) ? -1 : min;
+    }
+}
