ad164dea04ed54b3d725690ddcdfb07f4870a253
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-24 16:03:21 +0530

    Time: 11 ms (91.20%) | Memory: 42.4 MB (85.39%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,66 +1,25 @@
 class Solution {
-    // public int dfs(int i, int j, ArrayList<Integer> arr, int dp[][]) {
-    //     if (j < i)
-    //         return 0;
+    public int minCost(int N, int[] cuts) {
+        int n = cuts.length;
+        int[] arr = new int[n + 2];
+        arr[0] = 0;
+        arr[n + 1] = N;
 
-    //     if (dp[i][j] != -1)
-    //         return dp[i][j];
+        Arrays.sort(cuts);
+        for (int i = 1; i <= n; i++) arr[i] = cuts[i - 1];
 
-    //     int ans = Integer.MAX_VALUE;
-    //     for (int k = i; k <= j; k++) {
-    //         int cost = arr.get(j + 1) - arr.get(i - 1)
-    //                 + dfs(i, k - 1, arr, dp)
-    //                 + dfs(k + 1, j, arr, dp);
-    //         ans = Math.min(ans, cost);
-    //     }
-    //     return dp[i][j] = (ans == Integer.MAX_VALUE ? 0 : ans);
-    // }
+        int[][] dp = new int[n + 2][n + 2];
 
-    public int minCost(int n, int[] cuts) {
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // for (int c : cuts) arr.add(c);
-        // int m = arr.size();
-
-        // arr.add(0, 0);
-        // arr.add(n);
-        // Collections.sort(arr);
-
-        // int[][] dp = new int[m+1][m+1];
-        // for (int[] row : dp) Arrays.fill(row, -1);
-
-        // return dfs(1, m, arr, dp);
-
-        //tabulation-----------------
-
-        //base case
-        
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int c : cuts)
-            arr.add(c);
-        int m = arr.size();
-
-        arr.add(0, 0);
-        arr.add(n);
-        Collections.sort(arr);
-
-        int[][] dp = new int[m+ 2][m + 2];
-        for (int[] row : dp)
-            Arrays.fill(row, 0);
-
-        for (int i = m; i >= 1; i--) {
-            for (int j = 1; j <= m; j++) {
-                if (j < i)
-                    continue;
-                int ans = Integer.MAX_VALUE;
-                for (int k = i; k <= j; k++) {
-                    int cost = arr.get(j + 1) - arr.get(i - 1)
-                            + dp[i][k - 1]
-                            + dp[k + 1][j];
-                    ans = Math.min(ans, cost);
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j <= n; j++) {
+                int mini = Integer.MAX_VALUE;
+                for (int ind = i; ind <= j; ind++) {
+                    int cost = arr[j + 1] - arr[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                    mini = Math.min(mini, cost);
                 }
-                dp[i][j] = ans;
+                dp[i][j] = mini;
             }
         }
-        return dp[1][m];
+        return dp[1][n];
     }
 }
