3251e97adf28b31e7c7c79f6021406a6582c0bb1
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-29 00:47:28 +0530

    Time: 1 ms (83.13%) | Memory: 42.1 MB (69.28%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,27 +1,28 @@
 class Solution {
-    public boolean check(int []arr,int i ,int in){
-         while(i>=0 && i<arr.length){
-            if(arr[i]==0)i+=in;
-            else if(arr[i]>0){
-                arr[i]--;
-                in=-1*in;
-                i+=in;
-            }
-         }
-        for( i=0;i<arr.length;i++){
-            if(arr[i]!=0)return false;
-        }
-        // System.out.println();
-        return true;
-    }
     public int countValidSelections(int[] nums) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            //left
-            if(nums[i]==0 && check(nums.clone(),i,-1))c++;
-            //right
-            if(nums[i]==0 && check(nums.clone(),i,1))c++;
+        //optimal 
+        int n = nums.length;
+        int preSum[] = new int[n];
+
+        preSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            preSum[i] += preSum[i - 1] + nums[i];
         }
-        return c;
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                int leftSum = 0;
+                int RightSum = preSum[n - 1] - preSum[i];
+                if (i > 0)
+                    leftSum = preSum[i - 1];
+
+                if (leftSum == RightSum)
+                    count += 2;
+                else if (Math.abs(RightSum - leftSum) == 1)
+                    count++;
+            }
+        }
+        return count;
     }
 }
