de796198f0ec365756311e1bce1f31b81547dd89
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-10-29 00:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (48.19%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,21 +1,48 @@
+// class Solution {
+//     public int countValidSelections(int[] nums) {
+//         //optimal 
+//         int n = nums.length;
+//         int preSum[] = new int[n];
+
+//         preSum[0] = nums[0];
+//         for (int i = 1; i < n; i++) {
+//             preSum[i] += preSum[i - 1] + nums[i];
+//         }
+
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == 0) {
+//                 int leftSum = 0;
+//                 int RightSum = preSum[n - 1] - preSum[i];
+//                 if (i > 0)
+//                     leftSum = preSum[i - 1];
+
+//                 if (leftSum == RightSum)
+//                     count += 2;
+//                 else if (Math.abs(RightSum - leftSum) == 1)
+//                     count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+//2nd approach
 class Solution {
     public int countValidSelections(int[] nums) {
         //optimal 
-        int n = nums.length;
-        int preSum[] = new int[n];
+        int n = nums.length, sum = 0;
 
-        preSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            preSum[i] += preSum[i - 1] + nums[i];
-        }
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
 
         int count = 0;
+        int curr = 0;
         for (int i = 0; i < n; i++) {
+            curr += nums[i];
             if (nums[i] == 0) {
-                int leftSum = 0;
-                int RightSum = preSum[n - 1] - preSum[i];
-                if (i > 0)
-                    leftSum = preSum[i - 1];
+                int leftSum = curr;
+                int RightSum = sum - curr;
 
                 if (leftSum == RightSum)
                     count += 2;
