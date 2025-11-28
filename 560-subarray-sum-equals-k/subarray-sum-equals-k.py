9e6a27851d45bc6808ac2bf83afac35f2f90ddf6
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-11-28 11:48:45 +0530

    Time: 39 ms (34.28%) | Memory: 20.5 MB (22.74%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,12 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix=0
         hmap={0:1}
+        prefix=0
         res=0
-        for i in range(len(nums)):
-            prefix+=nums[i]
+        for num in nums:
+            prefix+=num
             diff=prefix-k
             if diff in hmap:
                 res+=hmap[diff]
             hmap[prefix]=1+hmap.get(prefix,0)
-        return res
+        print(hmap)        
+        return res            
+                
