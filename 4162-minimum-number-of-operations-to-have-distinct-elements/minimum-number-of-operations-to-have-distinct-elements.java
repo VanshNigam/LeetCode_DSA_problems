18084452e1c9096b5509cf88a6923b31b7e7f881
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-12-20 23:28:16 +0530

    Time: 10 ms (86.92%) | Memory: 139.9 MB (56.85%) - LeetSync

--- 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.java
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.java
@@ -1,24 +1,15 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        for (int i : nums)
-            mp.put(i, mp.getOrDefault(i, 0) + 1);
-
         int n = nums.length;
-        int i = 0, c = 0;
-        while (mp.size() != (n - i)) {
-            int k = 0;
-            while (i < n && k < 3) {
-                if (mp.get(nums[i]) != 1)
-                    mp.put(nums[i], mp.get(nums[i])-1);
-                else
-                    mp.remove(nums[i]);
-                i++;
-                k++;
-            }
-            c++;
-            // System.out.println(mp);
+        Set<Integer> set = new HashSet<>();
+
+        int i = n - 1;
+        while (i >= 0 && !set.contains(nums[i])) {
+            set.add(nums[i]);
+            i--;
         }
-        return c;
+
+        int idx = i + 1;
+        return (idx + 2) / 3;
     }
 }
