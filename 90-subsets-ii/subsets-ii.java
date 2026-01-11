b80255458bbe29fd62df1aabf9b944df4ad707f1
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2026-01-12 02:02:10 +0530

    Time: 2 ms (99.18%) | Memory: 45.5 MB (6.77%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,13 @@
 class Solution {
-
     public void help(int[] arr, List<Integer> temp, List<List<Integer>> result, int i) {
         if (i == arr.length) {
             result.add(new ArrayList<>(temp));
-            System.out.println(result);
             return;
         }
 
         temp.add(arr[i]);
         help(arr, temp, result, i + 1);
-         // Skip duplicates
-        while (i + 1 < arr.length && arr[i] == arr[i + 1]) {
-            i++;
-        }
+        while (i + 1 < arr.length && arr[i] == arr[i + 1])i++;
         temp.remove(temp.size() - 1);
         help(arr, temp, result, i + 1);
     }
@@ -21,11 +16,9 @@ class Solution {
         Arrays.sort(nums);
 
         List<List<Integer>> result = new ArrayList<>();
-
         List<Integer> temp = new ArrayList<>();
 
         help(nums, temp, result, 0);
-
         return result;
     }
 }
