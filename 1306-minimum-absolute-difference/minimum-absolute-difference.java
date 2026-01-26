94357764dab1a4fa4747a9d77aa8a4f7c8221a8a
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2026-01-26 13:51:27 +0530

    Time: 21 ms (72.27%) | Memory: 64.1 MB (32.26%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,21 +1,43 @@
+// class Solution {
+//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+//         int min=Integer.MAX_VALUE;
+//         int n=arr.length;
+//         List<List<Integer>>ans=new ArrayList<>();
+//         Arrays.sort(arr);
+
+//         for(int i=0;i<n-1;i++){
+//             // min=Math.min(min,Math.abs(arr[i],arr[i+1]));
+//             if(min>Math.abs(arr[i]-arr[i+1])){
+//                 min=Math.abs(arr[i]-arr[i+1]);
+//                 ans=new ArrayList<>();
+//                 ans.add(new ArrayList<>(Arrays.asList(arr[i], arr[i+1])));
+//             }else if(min==Math.abs(arr[i]-arr[i+1])){
+//                 ans.add(new ArrayList<>(Arrays.asList(arr[i], arr[i+1])));
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        int min=Integer.MAX_VALUE;
-        int n=arr.length;
-        List<List<Integer>>ans=new ArrayList<>();
+        List<List<Integer>> res = new ArrayList();
+        int len = arr.length;
+        int min = 1000000;
         Arrays.sort(arr);
+        for (int i = 0; i < len - 1; i++) {
+            min = Math.min(arr[i + 1] - arr[i], min);
+        }
+        for (int i = 0; i < len - 1; i++) {
+            if (arr[i + 1] - arr[i] == min) {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(arr[i]);
+                list.add(arr[i + 1]);
+                res.add(list);
 
-        for(int i=0;i<n-1;i++){
-            // min=Math.min(min,Math.abs(arr[i],arr[i+1]));
-            if(min>Math.abs(arr[i]-arr[i+1])){
-                min=Math.abs(arr[i]-arr[i+1]);
-                ans=new ArrayList<>();
-                ans.add(new ArrayList<>(Arrays.asList(arr[i], arr[i+1])));
-            }else if(min==Math.abs(arr[i]-arr[i+1])){
-                ans.add(new ArrayList<>(Arrays.asList(arr[i], arr[i+1])));
             }
         }
-
-        return ans;
+        return res;
     }
 }
